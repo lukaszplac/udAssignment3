f041216d8213414c47e4b4a4b46e23529d2b189c
Lukasz Plac <Lukasz.Plac@woodward.com>
2018-02-26 13:03:59 +0100

    assignment 3 done

---
+++ src/containers/Course/Course.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Course extends Component {
+
+	state = {
+		courseTitle: ''
+	}
+
+	componentDidMount() {
+		this.parseQueryParams();
+	}
+
+	componentDidUpdate() {
+		this.parseQueryParams();
+	}
+
+	parseQueryParams() {
+		const query = new URLSearchParams(this.props.location.search);
+		for (let params of query.entries()) {
+			if (this.state.courseTitle !== params[1]){
+				this.setState({
+					courseTitle: params[1]}
+					);
+			}
+		};
+	}
+
+    render () {
+        return (
+            <div>
+                <h1>{this.state.courseTitle}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
+            </div>
+        );
+    }
+}
+
+export default Course;
